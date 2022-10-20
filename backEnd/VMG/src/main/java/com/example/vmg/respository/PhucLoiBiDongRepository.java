35263efd7085a3ab9b3d0697a0b490eaaab19290
khangtranbc123 <{t.v.khang202@gmail.com}>
2022-10-20 22:02:24 +0700

    update

--- backEnd/VMG/src/main/java/com/example/vmg/respository/PhucLoiBiDongRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.vmg.respository;
-
-import com.example.vmg.model.PhucLoiBiDong;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PhucLoiBiDongRepository extends JpaRepository<PhucLoiBiDong, Long> {
-}
--- backEnd/VMG/src/main/java/com/example/vmg/service/PhongBanService.java
+++ backEnd/VMG/src/main/java/com/example/vmg/service/PhongBanService.java
@@ -1,7 +1,7 @@
 package com.example.vmg.service;
 
-import com.example.vmg.model.PhongBan;
-import com.example.vmg.respository.PhongBanRepository;
+import com.example.vmg.model.Department;
+import com.example.vmg.respository.DepartmentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,21 +10,21 @@ import java.util.List;
 @Service
 public class PhongBanService {
     @Autowired
-    private PhongBanRepository phongBanRepository;
+    private DepartmentRepository departmentRepository;
 
-    public List<PhongBan> getList(){
-        return phongBanRepository.findAll();
+    public List<Department> getList(){
+        return departmentRepository.findAll();
     }
 
-    public  PhongBan getById(Long id){
-        return phongBanRepository.findById(id).get();
+    public Department getById(Long id){
+        return departmentRepository.findById(id).get();
     }
 
-    public void saveOrUpDate(PhongBan phongBan){
-        phongBanRepository.save(phongBan);
+    public void saveOrUpDate(Department department){
+        departmentRepository.save(department);
     }
 
     public void delete(Long id){
-        phongBanRepository.deleteById(id);
+        departmentRepository.deleteById(id);
     }
 }
